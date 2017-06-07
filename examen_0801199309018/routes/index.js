905bdcb7cb5b5afb05753e85116748322f30de67
Osdy Ortiz <osdi@hotmail.es>
2017-06-06 19:38:58 -0600

    Los Cambios se ven reflejados en el routes/Index.js e views/index.hbs

--- examen_0801199309018/routes/index.js
+++ examen_0801199309018/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var datos={
+           titulo:'Alumno',
+
+           info:{NumeroCuenta:'0801199309018',Nombre:'Oscar Sady Gomez Ortiz', Correo:'osdi@hotmail.es'}
+        };
+
+  res.render('index', datos);
 });
+
 
 module.exports = router;
